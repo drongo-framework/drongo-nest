489f8ff93f1a1a325ccb9ae59e52a3ffda910c53
Sagar Chakravarthy <bp.sagar@gmail.com>
2017-05-28 19:04:28 +0530

    Integrating travis-ci.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
     ],
     install_requires=[
-        'uvloop',
+        'uvloop==0.8.0',
     ],
     packages=find_packages(),
     include_package_data=True,

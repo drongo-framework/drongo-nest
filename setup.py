8960eeaa552f591989f26261de87d26811aeb4af
Sattvik Chakravarthy <sattvik@gmail.com>
2017-07-12 13:57:11 +0530

    Updating setup scripts for first release.

--- setup.py
+++ setup.py
@@ -3,22 +3,31 @@
 from setuptools import find_packages, setup
 
 
+VERSION = '1.0.0'
+REPO_URL = 'https://github.com/drongo-framework/drongo-nest'
+DOWNLOAD_URL = REPO_URL + '/archive/v{version}.tar.gz'.format(version=VERSION)
+
 setup(
     name='drongo-nest',
-    version='1.0.0a1',
+    version=VERSION,
     description='High performance server for drongo.',
+    author='Sattvik Chakravarthy, Sagar Chakravarthy',
+    author_email='sattvik@gmail.com',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP',
     ],
     packages=find_packages(),
+    url=REPO_URL,
+    download_url=DOWNLOAD_URL,
     include_package_data=True,
     install_requires=[
         'six'

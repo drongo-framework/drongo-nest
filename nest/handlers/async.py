8d7fe2ccb0e3eb6ba7d6e0892fa5349b6a0fa7ab
Sattvik Chakravarthy <sattvik@gmail.com>
2017-11-02 16:13:05 +0530

    Adding more tracking and logging into async handler.

--- nest/handlers/async.py
+++ nest/handlers/async.py
@@ -1,9 +1,66 @@
 import asyncio
+import logging
+import time
 
 from ..parsers import HttpParser
 
 
 __all__ = ['AsyncHandler']
+
+
+class IOWrapper(object):
+    __slots__ = ['_real', '_conn', 'byte_count']
+
+    def __init__(self, real):
+        self._real = real
+        self._conn = None
+        self.byte_count = 0
+
+    def get_extra_info(self, *a, **kw):
+        return self._real.get_extra_info(*a, **kw)
+
+    def close(self):
+        self._conn.close()
+        self._real.close()
+
+    @asyncio.coroutine
+    def read(self, *a, **kw):
+        result = yield from self._real.read(*a, **kw)
+        self.byte_count += len(result)
+        return result
+
+    def write(self, *a, **kw):
+        self.byte_count += len(a[0])
+        return self._real.write(*a, **kw)
+
+
+class ConnectionWrapper(object):
+    __slots__ = ['_reader', '_writer', '_peer']
+
+    _logger = logging.getLogger('nest_connection')
+
+    def __init__(self, reader, writer):
+        self._reader = reader
+        self._writer = writer
+        reader._conn = self
+        writer._conn = self
+
+        self._peer = writer.get_extra_info('peername')
+
+        self._logger.info('New connection from [{peer}].'.format(
+            peer=self._peer
+        ))
+
+    def close(self):
+        self._logger.info('Disconnecting from [{peer}].'.format(
+            peer=self._peer
+        ))
+        self._logger.info(
+            'Received [{received} bytes] and send [{sent}] bytes.'.format(
+                received=self._reader.byte_count,
+                sent=self._writer.byte_count
+            )
+        )
 
 
 class Reader(object):
@@ -50,6 +107,8 @@ class Responder(object):
 
 
 class AsyncHandler(object):
+    _logger = logging.getLogger('nest')
+
     def __init__(self, nest, app, socket):
         self._clients = {}
 
@@ -58,6 +117,10 @@ class AsyncHandler(object):
         self.sock = socket
 
     def accept(self, reader, writer):
+        reader = IOWrapper(reader)
+        writer = IOWrapper(writer)
+        ConnectionWrapper(reader, writer)
+
         self._ip, self._port = writer.get_extra_info('peername')
 
         task = self.loop.create_task(self.handle(reader, writer))
@@ -76,6 +139,7 @@ class AsyncHandler(object):
         while True:
             try:
                 env = yield from http.get_one()
+                timer_start = time.time()
                 if env is None:
                     break
 
@@ -83,6 +147,10 @@ class AsyncHandler(object):
                 env['CLIENT_PORT'] = self._port
 
                 yield from responder.respond(env)
+                timer_end = time.time()
+                self._logger.info('Executed in [{time:0.3f} ms]'.format(
+                    time=(timer_end - timer_start) * 1000
+                ))
             except ConnectionResetError:
                 break  # Ignore the connection error
             except BrokenPipeError:
